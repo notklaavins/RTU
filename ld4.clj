1b0316e9525fb3a7fac18bfa525f56dd40bf9ff9
notklaavins <55324998+notklaavins@users.noreply.github.com>
2020-01-22 16:22:53 +0200

    LD 4

---
+++ ld4.clj
@@ -0,0 +1,25 @@
+(defn avgMark
+    
+    [Map]
+
+    (def wordsWithA (reduce into []
+            (filter 
+            #(= "a" (subs (first %) (- (count (first %)) 1))) Map) 
+        )
+    )
+  
+    (def markSum (loop [sum 0 i 1]
+        (if (>= i (count wordsWithA))
+            sum
+            (recur (+ sum (nth wordsWithA i)) (+ i 2))
+            )
+        )
+    )       
+  
+  (print "Result: ")
+  
+  (print (float (/ markSum (/ (count wordsWithA) 2))))
+  
+)
+
+(avgMark {"Raimonds" 2 "Petrovs" 6 "Maris" 8 "Natalija" 5 "Peteris" 10 "Marta" 2 "Sandra" 1 "Krišjanis" 9})          ; (5+2+1)/3 = 2.6666...
